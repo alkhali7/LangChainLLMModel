ff8942ae986c98e2d8f254aec6f75422087b953b
Shams Alkhalidy <alkhali7@msu.edu>
2025-09-03 16:53:41 -0400

    updated w llamallm

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-from dotenv import load_dotenv
 # import os  # to access environment variables
+
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+# from langchain_openai import ChatOpenAI
+from langchain_community.llms import Ollama  # instead of ChatOpenAI
+
 load_dotenv()  # take environment variables from .env.
 
 
 def main():
     print("Hello from langchain-course!")
     # print(os.environ.get("OPENAI_API_KEY" ))
-
+    information = """"
+        Elon Musk
+        Musk in 2022
+Senior Advisor to the Presidentarture from the Department of Government Efficiency
+        Recorded May 30, 2025. This article is part of a series about June 28, 1971) is an international businessman and entrepreneur known for his leadership of Tesla, SpaceX, X (formerly Twitter), and the Department of Government Efficiency (DOGE). Musk has been the wealthiest person in the world since 2021; as of May 2025, Forbes estimates his net worth to be US$424.7 billion.
+        Born to a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he had obtained Canadian citizenship at birth through his Canadian-born mother. He received bachelor's degrees in 1997 from the University of Pennsylvania in Philadelphia, United States, before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became an American citizen.
+    """
+    summary_template = """ 
+    Given the following information {information} about a person, I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+    
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"] , template=summary_template)
+    
+    llm = Ollama(temperature=0, model='llama3')  # using Ollama instead of ChatOpenAI
+    chain = summary_prompt_template | llm  # creating a chain with the prompt and the LLM
+    response = chain.invoke(input={"information": information}) # invoking the chain with the information
+    
+    print(response)  # printing the response
+    
 if __name__ == "__main__":
     main()
