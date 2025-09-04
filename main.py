8d9d574fc1c1c1d1d1c78c999de53bda8a6fb34c
Shams Alkhalidy <alkhali7@msu.edu>
2025-09-04 10:44:22 -0400

    hello-world-chain

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-# import os  # to access environment variables
+import os  # to access environment variables
 
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 # from langchain_openai import ChatOpenAI
-from langchain_community.llms import Ollama  # instead of ChatOpenAI
+# from langchain_ollama import OllamaLLM  # instead of ChatOpenAI
+from langchain_ollama import ChatOllama
+
 
 load_dotenv()  # take environment variables from .env.
 
@@ -11,10 +13,9 @@ load_dotenv()  # take environment variables from .env.
 def main():
     print("Hello from langchain-course!")
     # print(os.environ.get("OPENAI_API_KEY" ))
-    information = """"
+    information = """
         Elon Musk
-        Musk in 2022
-Senior Advisor to the Presidentarture from the Department of Government Efficiency
+        Musk in 2022 Senior Advisor to the Presidentarture from the Department of Government Efficiency
         Recorded May 30, 2025. This article is part of a series about June 28, 1971) is an international businessman and entrepreneur known for his leadership of Tesla, SpaceX, X (formerly Twitter), and the Department of Government Efficiency (DOGE). Musk has been the wealthiest person in the world since 2021; as of May 2025, Forbes estimates his net worth to be US$424.7 billion.
         Born to a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he had obtained Canadian citizenship at birth through his Canadian-born mother. He received bachelor's degrees in 1997 from the University of Pennsylvania in Philadelphia, United States, before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became an American citizen.
     """
@@ -27,11 +28,11 @@ Senior Advisor to the Presidentarture from the Department of Government Efficien
     summary_prompt_template = PromptTemplate(
         input_variables=["information"] , template=summary_template)
     
-    llm = Ollama(temperature=0, model='llama3')  # using Ollama instead of ChatOpenAI
+    llm = ChatOllama(temperature=0, model='gemma3:270m')  # using Ollama instead of ChatOpenAI
     chain = summary_prompt_template | llm  # creating a chain with the prompt and the LLM
     response = chain.invoke(input={"information": information}) # invoking the chain with the information
     
-    print(response)  # printing the response
+    print(response.content)  # printing the response
     
 if __name__ == "__main__":
     main()
